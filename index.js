a07187445308a8731957a428a3a7261a96932dd5
ijaz35 <ijaz.hossain1@gmail.com>
2023-08-29 13:33:07 +0600

    menu related api added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrkqnje.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,9 +24,47 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const menuCollection = client.db("bistroDBNew").collection("menu");
+        const reviewsCollection = client.db("bistroDBNew").collection("reviews");
+        const cartCollection = client.db("bistroDBNew").collection("cart");
+        const userCollection = client.db("bistroDBNew").collection("user");
+        // user related APIs
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result)
+        })
+        // menu related APIs
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+        })
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            // console.log(email);
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            console.log(cartItem);
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result)
 
+        })
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
             res.send(result);
         })
         // Send a ping to confirm a successful connection
