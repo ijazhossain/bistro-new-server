2a7c81850d1c851e236311384c96ed51372f5ea3
ijaz35 <ijaz.hossain1@gmail.com>
2023-08-31 17:30:52 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access1' })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access2' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrkqnje.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,6 +43,11 @@ async function run() {
         const reviewsCollection = client.db("bistroDBNew").collection("reviews");
         const cartCollection = client.db("bistroDBNew").collection("cart");
         const userCollection = client.db("bistroDBNew").collection("user");
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
         // user related APIs
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -64,9 +85,13 @@ async function run() {
             const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             // console.log(email);
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(401).send({ error: true, message: 'forbidden access' })
+            }
             if (!email) {
                 res.send([])
             }
